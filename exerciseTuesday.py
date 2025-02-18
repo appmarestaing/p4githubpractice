33a9edb9663d34aa3162175f42d045ffc49e15e6
appmarestaing marestaing <marestaing.noah@pusd.us>
2025-02-18 00:38:31 +0000

    notes

---
+++ exerciseTuesday.py
@@ -0,0 +1,36 @@
+# Create a function that asks for someones birth year and then prints out how old they will be in 2025.
+
+
+
+
+# Math stuff
+a = 100
+b = 200 
+c = 9 * a
+d = b / 2
+print(c)
+print(a ** 2)
+
+# Does anyone know how we could convert this back into an integer?
+print(d)
+
+
+# Input and Output
+num1 = input("what number do you want?")
+print(num1 * 2)
+
+
+# Creating a variable
+# What are these called where it's like a word in quotes?
+first_name = "noah"
+last_name = "marestaing"
+
+# How would we make it so that they are spaced out?
+full_name = first_name + last_name
+print(full_name)
+
+# How would we ask the user instead of just always printing out the same name?
+
+# How could we make that into a function?
+
+
